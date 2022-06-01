2264e88e264ed6d174dbb658a6e1b27944e5e458
FilippoMarinelli <lipo-x123@outlook.com>
2022-06-01 10:32:29 -0300

    Firt class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		Position a = new Position(4, 5);
+		System.out.println(a.toString());
 
 	}
 
